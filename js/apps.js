e599714a62762a47e1a251215c58f7ddb0ad672c
Bateyjosue <josuebatey19@gmail.com>
2022-08-02 10:27:13 +0200

    Update Book Collection from Array Objects to Class Methods

--- js/apps.js
+++ js/apps.js
@@ -9,6 +9,21 @@ class Book {
     this.author = author;
     this.id = null;
   }
+
+  createBook = (title, author) => {
+    bookArray.push({ title, author });
+  }
+
+  removeBook = (book) => {
+    const bookTitle = book.querySelector('.title').innerHTML;
+    const filt = bookArray.filter((book) => bookTitle.toLowerCase() === book.title.toLowerCase());
+    const filtIndex = bookArray.indexOf(filt[0]);
+    bookArray.splice(filtIndex, 1);
+  }
+
+  updateCollection = () => {
+    localStorage.setItem('bookArray', JSON.stringify(bookArray));
+  }
 }
 
 if (localStorage.getItem('bookArray') === null) {
@@ -17,33 +32,34 @@ if (localStorage.getItem('bookArray') === null) {
   bookArray = JSON.parse(localStorage.getItem('bookArray'));
 }
 
-function createBook(title, author) {
-  const newBook = new Book(title, author);
-  bookArray.push(newBook);
-  localStorage.setItem('bookArray', JSON.stringify(bookArray));
-}
-
+const bookOject = new Book();
 addBookButton.addEventListener('click', (event) => {
   event.preventDefault();
   const bookTitle = addBook.querySelector('input[name="title"]').value;
   const bookAuthor = addBook.querySelector('input[name="author"]').value;
-  createBook(bookTitle, bookAuthor);
-  bookList.innerHTML += `<li class="book">
-        <p class="title">${bookTitle}</p>
-        <p class="author">${bookAuthor}</p>
-        <button type="button" class="delete-btn">Remove</button>
-        <hr />
-      </li>`;
+  if (bookTitle.length > 1 && bookAuthor.length > 1) {
+    bookOject.createBook(bookTitle, bookAuthor);
+    bookOject.updateCollection();
+    bookList.innerHTML += `<li class="book">
+          <p class="title">${bookTitle}</p>
+          <p class="author">${bookAuthor}</p>
+          <button type="button" class="delete-btn">Remove</button>
+          <hr />
+        </li>`;
+  } else {
+    const div = document.createElement('div');
+    div.className = 'validation-error';
+    div.textContent = 'Please Book Title or Book Author Shouldn\'t be empty';
+    div.style.display = 'block';
+    addBook.appendChild(div);
+  }
 });
 
 bookList.addEventListener('click', (e) => {
   if (e.target.className === 'delete-btn') {
     const book = e.target.parentElement;
-    const bookTitle = book.querySelector('.title').innerHTML;
-    const filt = bookArray.filter((book) => bookTitle.toLowerCase() === book.title.toLowerCase());
-    const filtIndex = bookArray.indexOf(filt[0]);
-    bookArray.splice(filtIndex, 1);
-    localStorage.setItem('bookArray', JSON.stringify(bookArray));
+    bookOject.removeBook(book);
+    bookOject.updateCollection();
     bookList.removeChild(book);
   }
 });
